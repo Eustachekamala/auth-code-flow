d734d4048f5ee0e86af30ef7e99535de81882574
eustachekamala <eustachejared25@gmail.com>
2025-08-16 01:07:08 +0300

    security demos with keycloack

---
+++ src/main/java/com/eustache/auth_code_flow/HomeController.java
@@ -0,0 +1,25 @@
+package com.eustache.auth_code_flow;
+
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping
+    public String home(OAuth2AuthenticationToken token) {
+        if (token == null) {
+            return "You are not authenticated. Please log in using OAuth2.";
+        }
+        String name = token.getName();
+        String email = token.getPrincipal().getAttribute("email");
+        String role = token.getPrincipal().getAuthorities().iterator().next().getAuthority();
+        String authorities = token.getAuthorities().toString();
+        System.out.println(name + " " + email + " " + role + " " + authorities);
+        return "Welcome to the OAuth2 Authorization Code Flow Example!" +
+               " User: " + name +
+                " Email: " + email +
+                " Role: " + role+
+               " with authorities: " + authorities;
+    }
+}
