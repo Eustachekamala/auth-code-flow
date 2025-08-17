928dc485756b9c064e4444b78d1f5eee6cfa3c95
eustachekamala <eustachejared25@gmail.com>
2025-08-17 11:46:29 +0300

    added the client module

---
+++ pure-client/src/test/java/com/eustache/pureclient/PureClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eustache.pureclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PureClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
